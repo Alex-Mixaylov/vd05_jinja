6cdf69baa732ef8e7cd17dc20a7625d82d6f25c5
Alex <guardamar81@gmail.com>
2024-07-24 23:04:22 +0400

    DZ
    DZ

--- app/routes.py
+++ app/routes.py
@@ -29,3 +29,18 @@ def index():
         if title and content:
             posts.append({'title': title, 'content': content})
     return render_template('blog.html', posts=posts, **context)
+
+#Домашнее задание VD06
+@app.route("/dz01_vd06/", methods=["GET", "POST"])
+def dz01_vd06():
+    context = {
+        "link": "Сохранить данные" # Выводится надпись на кнопке отправки формы
+    }
+    if request.method == 'POST':
+        name = request.form.get('name')
+        age = request.form.get('age')
+        city = request.form.get('city')
+        hobby = request.form.get('hobby')
+        if name and age and city and hobby:
+            posts.append({'name': name, 'age': age, 'city': city, 'hobby': hobby})
+    return render_template('dz01_vd06.html', posts=posts, **context)
