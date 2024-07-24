373cbb4cb389767af8693ec69a834e838167d0ad
Alex <guardamar81@gmail.com>
2024-07-24 21:51:03 +0400

    1
    1

---
+++ app/routes.py
@@ -0,0 +1,34 @@
+from flask import render_template, request, redirect, url_for
+from app import app
+
+posts =[]
+
+@app.route("/")
+def site_launch():
+    contex = {
+        "link": "Подробнее о туре"
+    }
+    return render_template("index.html", **contex)
+
+@app.route("/shablon/")
+def sablon():
+    return render_template("shablon.html")
+
+@app.route("/contacts/")
+def contacts():
+    return render_template("contacts.html")
+
+@app.route("/blog/", methods=["GET", "POST"])
+def index():
+    if request.method =='POST':
+        title = request.form.get('title')
+        content = request.form.get('content')
+        if title and content:
+            posts.append({'title': title, 'content': content})
+    return render_template('blog.html', posts=posts)
+
+def blog():
+    contex = {
+        "link": "Опубликовать пост"
+    }
+    return render_template("blog.html", **contex)
--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-@app.route("/")
-def site_launch():
-    contex = {
-        "link": "Подробнее о туре"
-    }
-    return render_template("index.html", **contex)
-
-@app.route("/shablon/")
-def sablon():
-    return render_template("shablon.html")
-
-@app.route("/blog/")
-def blog():
-    contex = {
-        "link": "Купить тур"
-    }
-    return render_template("blog.html", **contex)
-
-@app.route("/contacts/")
-def contacts():
-    return render_template("contacts.html")
+from app import app
 
 # Запуск сайта
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
